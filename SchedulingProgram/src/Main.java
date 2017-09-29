75481811dd14ed80e0005609468aec975f424a6f
benjer3 <benjer3@outlook.com>
2017-09-29 13:29:15 -0400

    Added Main

---
+++ SchedulingProgram/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+	public void main() {
+		
+	}
+	
+	public void addEmployeeToEvent(Employee e, Event a){
+		
+	}
+	
+	public void swapEventEmployees(Employee e1, Employee e2, Event a1, Event a2){
+		
+	}
+	
+	public void employeeBlacklistVenue(Employee e, Venue v){
+		
+	}
+	
+	public void employeeFavorVenue(Employee e, Venue v){
+		
+	}
+
+	public void venueBlacklistEmployee(Employee e, Venue v){
+	
+	}
+	public void venueFavorEmployee(Employee e, Venue v){
+	
+	}
+	public String requestAbsence(Employee e, Event a){
+		//returns an "absence request form" of type string
+		return "absence request #";
+	}
+	public String requestAbsence(Employee e, TimeSlot a){
+		//returns an "absence request form" of type string
+		return "absence request #";
+	}
+	public String changeSchedule(Employee e, Event a){
+		//returns an "schedule change form" of type string
+		return "schedule change #";
+	}
+	public String changeSchedule(Employee e, TimeSlot a){
+		//returns an "schedule change form" of type string
+		return "schedule change #";
+	}
+	public String dismissRequest(String request){
+		return "accept/reject";
+	}
+}
