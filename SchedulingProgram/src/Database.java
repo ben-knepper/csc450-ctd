479c38f9ad56756dc265f402aeedd1e794ae9d2c
benjer3 <benjer3@outlook.com>
2017-10-13 09:25:43 -0400

    Added .gitignore

---
+++ SchedulingProgram/src/Database.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+public final class Database
+{
+	private Database() { }
+	
+	public static void read(String query, String... args)
+	{
+		
+	}
+}
--- SchedulingProgram/src/Manager.java
+++ SchedulingProgram/src/Manager.java
@@ -1,4 +1,10 @@
 
 public class Manager extends Employee {
 
+	public Manager(String f, String l, String i, String p, String a)
+	{
+		super(f, l, i, p, a);
+		// TODO Auto-generated constructor stub
+	}
+
 }
