4c9e4063ee3aedf758179e67b551bac9bc79c37c
Freddy <xvnobody@yahoo.com>
2017-11-02 10:52:42 -0400

    Edited Add Procedures

--- Database/add_procedures.sql
+++ Database/add_procedures.sql
@@ -11,6 +11,20 @@ drop procedure if exists AddVenue;
 drop procedure is exists UpdateVenue;
 drop procedure if exists SearchVenue;
 
+drop procedure if exists AddBlacklisted;
+drop procedure if exists SearchBlacklistedEmployee;
+drop procedure if exists SearchBlacklistedVenue;
+
+drop procedure if exists AddVenueDateTime;
+drop procedure if exists SearchVenueDateTime;
+
+drop procedure if exists AddScheduled;
+drop procedure if exists SearchScheduledEmployee;
+drop procedure if exists SearchScheduledVenue;
+
+drop procedure if exists AddAvailability;
+drop procedure if exists SearchAvailability;
+
 delimiter //
 
 create procedure GetTable(
@@ -115,6 +129,103 @@ create procedure SearchVenue(
 begin
 	select * from venue
 		where v_name = name;
+end//
+
+/*Blacklisted Procedures*/
+create procedure AddBlacklisted(
+	in e_id varchar(10),
+	in v_id varchar(10)
+)
+begin
+	insert into blacklisted values(
+		e_id, v_id
+	);
+end//
+
+create procedure SearchBlacklistedEmployee(
+	in e_id varchar(10)
+)
+begin
+	select venueID from blacklisted
+		where e_id = employeeID;
+end//
+
+create procedure SearchBlacklistedVenue(
+	in v_id varchar(10)
+)
+begin
+	select employeeID from blacklisted
+		where v_id = venueID;
+end//
+
+/*Venue Date Time Procedures*/
+create procedure AddVenueDateTime(
+	in v_id varchar(10),
+	in s_date_time datetime,
+	in e_date_time datetime,
+	in is_open boolean
+)
+begin
+	insert into venue_date_time values(
+		v_id, s_date_time, e_date_time, is_open
+	);
+end//
+
+create procedure SearchVenueDateTime(
+	in v_id varchar(10)
+)
+begin
+	select * from venue_date_time
+		where v_id = venueID;
+end//
+
+/*Scheduled Procedures*/
+create procedure AddScheduled(
+	in e_id varchar(10),
+	in v_id varchar(10),
+	in s_date_time datetime
+)
+begin
+	insert into scheduled values(
+		e_id, v_id, s_date_time
+	);
+end//
+
+create procedure SearchScheduledEmployee(
+	in e_id varchar(10)
+)
+begin
+	select * from scheduled
+		where e_id = employeeID;
+end//
+
+create procedure SearchScheduledVenue(
+	in v_id varchar(10)
+)
+begin
+	select * from scheduled
+		where v_id = venueID;
+end//
+
+/*Availability Procedures*/
+create procedure AddAvailability(
+	in e_id varchar(10),
+	in weekday varchar(10),
+	in s_time time,
+	in e_time time
+)
+begin
+	insert into availability values(
+		e_id, weekday, s_time, e_time
+	);
+end//
+
+create procedure SearchAvailability(
+	in e_id varchar(10)
+)
+begin
+	select * from availability
+		where e_id = employeeID;
 end//
 
 delimiter ;
