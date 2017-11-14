f1cfb8e71e4e8be3c19664d2d20c26a10c9bc604
gsm4553 <32642320+gsm4553@users.noreply.github.com>
2017-11-14 16:19:16 -0500

    Update TimeSlot.java

--- SchedulingProgram/src/TimeSlot.java
+++ SchedulingProgram/src/TimeSlot.java
@@ -1,4 +1,25 @@
 
 public class TimeSlot {
-	private int start1, start2, end1, end2;
+	//Each time slot contains an numerical identifier, the hour of the day
+	//(in military time) times the day of the week times 24
+	//(Monday is 0, Tuesday is 1, Sunday is 6.)
+	private int id;
+	private String day, hour;
+	public TimeSlot(int h, int d){
+		id = d*24+h;
+	}
+
+	public TimeSlot(int iden){
+		id = iden;
+	}
+	public boolean isEqual(TimeSlot t){
+		return true;
+	}
+	public int getId(){
+		return id;
+	}
+	public String toString(){
+		return day + " " + hour;
+	}
+	
 }
