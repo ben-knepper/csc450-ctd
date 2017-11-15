a15beebef07c929c33d49ed2175a65e33df0ad80
Classical <gusinscbo@student.gu.se>
2017-11-15 15:52:04 -0500

    Basic Table View edit to Viewer Class

--- SchedulingProgram/src/Viewer.java
+++ SchedulingProgram/src/Viewer.java
@@ -1,98 +1,50 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.util.ArrayList;
+import java.awt.FlowLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
+import javax.swing.border.MatteBorder;
+import javax.swing.*;
 import net.miginfocom.swing.MigLayout;
-import javax.swing.JButton;
 
-public class Viewer {
-	Database db = new Database();
-	ArrayList<Employee> employees;
-	
-	
-	JButton[] btnEmployees;
-	
-	private JFrame frame;
-	private JPanel panel = new JPanel();
-
-	JLabel lblName = new JLabel("Name:");
-	JLabel lblLastName = new JLabel("Last Name:");
-
-	JTextField txtFirstName = new JTextField(20);
-	JTextField txtLastName = new JTextField(20);
-
-	JLabel lblID = new JLabel("Employee ID:");
-	JLabel lblPhone = new JLabel("Phone Number:");
-	JLabel lblEmail = new JLabel("Email:");
-	JLabel lblAddress = new JLabel("Address");
-	private final JLabel lblEmployee = new JLabel("Employee 1");
-	private final JLabel lblEmployee_1 = new JLabel("Employee 2");
-
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			
-			public void run() {
-				try {
-					Viewer window = new Viewer();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+public class Viewer extends JFrame {
+	JFrame frame;
+	JPanel panel = new JPanel();
+	JButton[] btnEmployees = new JButton[40];
+	JLabel[] columnTitles;
+	String[] daysOfTheWeek = {"Name", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 	/**
 	 * Create the application.
 	 */
 	public Viewer() {
-		initialize();
-	}
-	
-	public void populateNames(){
-		// Convert string to JLabel to load into frame.
-		for(int i = 0; i <= employees.size(); i++){
-		};
-		
-	}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocation(0, 0);
+		setSize(new Dimension(600,800));
+		setLayout(new MigLayout(""));
+		panel.setLayout(new MigLayout(""));
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new MigLayout("", "[][][][][][]", "[][]"));
-		
-		JLabel lblName_1 = new JLabel("Name");
-		frame.getContentPane().add(lblName_1, "cell 0 0");
-		for(int i = 0; i < 40; i++){
-			btnEmployees [i] = new JButton("Employee " + i);
-			frame.getContentPane().add(btnEmployees[i], "cell 0 " + i);
+		System.out.println(daysOfTheWeek[2]);
+
+		columnTitles = new JLabel[daysOfTheWeek.length];
+		for(int i = 0; i < daysOfTheWeek.length;i++){
+			columnTitles[i] = new JLabel(daysOfTheWeek[i]);
+			panel.add(columnTitles[i]);
 		}
-		JLabel lblPhone_1 = new JLabel("Phone");
-		frame.getContentPane().add(lblPhone_1, "cell 1 0");
+		panel.add(new JLabel(""),"wrap");
+		for (int i = 0; i < 40; i++) {
+			btnEmployees[i] = new JButton("Employee " + (i + 1));
+			panel.add(btnEmployees[i],"wrap");
+		}
+		System.out.println(btnEmployees[0].getText());
 		
-		JLabel lblJanuary = new JLabel("January 1");
-		frame.getContentPane().add(lblJanuary, "cell 2 0");
-		
-		
-		JLabel label = new JLabel("9196107512");
-		frame.getContentPane().add(label, "cell 1 1");
-		
-		JLabel lblJohnScotts = new JLabel("John Scott's");
-		frame.getContentPane().add(lblJohnScotts, "cell 2 1");
-		//frame.getContentPane().add(panel, "cell 0 0");
-
+		panel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.gray));
+		add(panel);
+		setVisible(true);
 	}
+
+	public static void main(String[] args) {
+		Viewer viewer = new Viewer();
+	};
 
 }
