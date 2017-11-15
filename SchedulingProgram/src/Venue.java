68a164348bcaf258d2963f4c0420ad18423657c7
gsm4553 <32642320+gsm4553@users.noreply.github.com>
2017-11-15 09:43:22 -0500

    Update Venue.java

--- SchedulingProgram/src/Venue.java
+++ SchedulingProgram/src/Venue.java
@@ -1,47 +1,79 @@
 
 public class Venue {
-	private String id;
+	private String ID;
 	private String name;
-	private int tables;
 	private String address;
+	private int tables;
 	private TimeSlot[] times; 
 	
-	public Venue(String i, String n, int t, String a) {
-		id = i;
-		name = n;
-		address = a;
-		tables = t;	
-	}
-
-	public String getID() {
-		return id;
-	}
-
-	public void setID(String id) {
-		this.id = id;
-	}
 	
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
 	
-	public int getTables() {
-		return tables;
-	}
 	
-	public void setTables(int tables) {
-		this.tables = tables;
-	}
+public Venue(String id, String n, String a, int t) {
+	ID = id;
+	name = n;
+	address = a;
+	tables = t;
+	
+}
+
+
+
+
+public String getID() {
+	return ID;
+}
+
+
+
+
+public void setID(String iD) {
+	ID = iD;
+}
+
+
+
+
+public String getName() {
+	return name;
+}
+
+
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+
+
+public String getAddress() {
+	return address;
+}
+
+
+
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+
+
+
+public int getTables() {
+	return tables;
+}
+
+
+
+
+public void setTables(int tables) {
+	this.tables = tables;
+}
+
+public String toString() {
+	return this.name;
+}
 }
