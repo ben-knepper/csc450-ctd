19e98da28e0afe1607d2c6803d9d192460a6f154
gsm4553 <32642320+gsm4553@users.noreply.github.com>
2017-11-27 09:38:02 -0500

    Added Time Slot Dictionary and Documentation

--- SchedulingProgram/src/Venue.java
+++ SchedulingProgram/src/Venue.java
@@ -1,52 +1,101 @@
-
+import java.util.Dictionary;
+//Object containing a Venue's scheduling and physical information.
 public class Venue {
 	private String ID;
 	private String name;
 	private String address;
 	private int tables;
-	private TimeSlot[] times; 
-		
-	public Venue(String id, String n, int t, String a) {
-		ID = id;
-		name = n;
-		tables = t;
-		address = a;
-		
+	private Dictionary<TimeSlot, String> times;
+
+public Venue(String id, String n, String a, int t) {
+	//constructor
+	ID = id;
+	name = n;
+	address = a;
+	tables = t;
+	times = buildTimeSlots();
+}
+
+public Dictionary<TimeSlot, String> buildTimeSlots(){
+	//creates a dictionary of time slots, each of which contains a "yes" or "no" value.
+	for (int h = 0; h < 168; h++){
+		times.put(new TimeSlot(h), null);
 	}
-	
-	public String getID() {
-		return ID;
-	}
-	
-	public void setID(String iD) {
-		ID = iD;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getTables() {
-		return tables;
-	}
-	
-	public void setTables(int tables) {
-		this.tables = tables;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
-	
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
-	public String toString() {
-		return this.name;
-	}
+	return times;
+
+
+}
+
+
+
+
+public String getID() {
+	return ID;
+}
+
+
+
+
+public void setID(String iD) {
+	ID = iD;
+}
+
+
+
+
+public String getName() {
+	return name;
+}
+
+
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+
+
+public String getAddress() {
+	return address;
+}
+
+
+
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+
+
+
+public int getTables() {
+	return tables;
+}
+
+
+
+
+public void setTables(int tables) {
+	this.tables = tables;
+}
+
+public String toString() {
+	return this.name;
+}
+
+public Dictionary<TimeSlot, String> getTimes() {
+	return times;
+}
+
+
+
+
+public void setTimes(Dictionary<TimeSlot, String> times) {
+	this.times = times;
+}
+
+
 }
