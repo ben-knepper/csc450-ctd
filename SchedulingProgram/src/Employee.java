b1993889855a572f7ae4823a91139cdf75e59591
gsm4553 <32642320+gsm4553@users.noreply.github.com>
2017-11-27 09:35:25 -0500

    Added Time Slot Dictionary and documentation

--- SchedulingProgram/src/Employee.java
+++ SchedulingProgram/src/Employee.java
@@ -1,101 +1,134 @@
+import java.util.Dictionary;
+//Object containing an Employee's scheduling and contact information.
 
 public class Employee {
-	private String id;
 	private String firstName;
+	private String id;
 	private String lastName;
-	private String password;
 	private String phone;
+	private String address;
+	private String password;
 	private String email;
 	private boolean manager;
-	private TimeSlot[] times;
+	private Dictionary<TimeSlot, String> times;
 
-	public Employee(String i, String f, String l, String ip, String p, String e) {
-		id = i;
-		firstName = f;
-		lastName = l;
-		password = ip;
-		phone = p;
-		email = e;
-		manager = false;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
+public Employee(String f, String l, String i,
+		String p, String a, String ip, String e){
+	//constructor
+	firstName = f;
+	lastName = l;
+	id = i;
+	phone = p;
+	address = a;
+	password = ip;
+	manager = false;
+	email = e;
+	times = buildTimeSlots();
+    }
 
-	public void setPassword(String pw) {
-		this.password = pw;
+public Dictionary<TimeSlot, String> buildTimeSlots(){
+	//creates a dictionary of time slots, each of which contains a "yes" or "no" value.
+	for (int h = 0; h < 168; h++){
+		times.put(new TimeSlot(h), "n");
 	}
-
-	public String getPhone() {
-		return phone;
-	}
+	return times;
 	
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
 	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public boolean isManager() {
-		return manager;
-	}
-	
-	public void setManager(boolean manager) {
-		this.manager = manager;
-	}
-
-	public void addTimes(TimeSlot t){
-		int iden = t.getId();
-
-		times.put(new TimeSlot(iden), "y");
-	}
-	public void removeTimes(TimeSlot t){
-		int iden = t.getId();
-
-		times.put(new TimeSlot(iden), "n");
-	}
-	public void blockTimes(TimeSlot t){
-		int iden = t.getId();
-
-		times.put(new TimeSlot(iden), "b");
 }
 
-	public TimeSlot[] getTimes() {
-		return times;
-	}
-	
-	public String toString() {
-		return(this.firstName + " " + this.lastName + " " + id);
-	}
+public String getEmail() {
+	return email; 
+}
+
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getPhone() {
+	return phone;
+}
+
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public boolean isManager() {
+	return manager;
+}
+
+public void setManager(boolean manager) {
+	this.manager = manager;
+}
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String pw) {
+	this.password = pw;
+}
+
+public void addTimes(TimeSlot t){
+	//Sets an existing time slot to "yes".
+	int iden = t.getId();
+
+	times.put(new TimeSlot(iden), "y");
+}
+public void removeTimes(TimeSlot t){
+
+	//Sets an existing time slot to "no".
+	int iden = t.getId();
+
+	times.put(new TimeSlot(iden), "n");
+}
+public void blockTimes(TimeSlot t){
+
+	//Sets an existing time slot to "blocked" - it will be considered as a "no" temporarily.
+	int iden = t.getId();
+
+	times.put(new TimeSlot(iden), "b");
+}
+
+public Dictionary<TimeSlot, String> getTimes() {
+	return times;
+}
+public String toString() {
+	return(this.firstName + " " + this.lastName + " " + id);
+}
+
+
+
 }
