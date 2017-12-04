8a3346527c232e6854b7f65c6224b036dfce9390
gsm4553 <32642320+gsm4553@users.noreply.github.com>
2017-12-04 12:10:26 -0500

    Added documentation
    I assume you took a lot of the functions out and put it elsewhere.

--- SchedulingProgram/src/Event.java
+++ SchedulingProgram/src/Event.java
@@ -1,25 +1,36 @@
 public class Event {
-
+	/**Each Event contains a venue, the number of tables at the venue, 
+	* and a single employee.
+	*/
 	private Venue venue;
 	private Employee employee; // assuming a small number (<10)
 	private int tables;
 	private boolean found;
 
 	public Event(Venue v, Employee e) {
+		
+		/** constructor
+		*/ 
 		venue = v;
 		employee = e;
 	}
 
 
 	public int getTables() {
+		/** Gets the number of tables.
+		*/ 
 		return tables;
 	}
 
 	public void setTables(int tables) {
+		/** Sets the number of tables.
+		*/ 
 		this.tables = tables;
 	}
 
 	public Venue getVenue() {
+		/** Gets the venue.
+		*/ 
 		return venue;
 	}
 
@@ -27,6 +38,8 @@ public class Event {
 
 
 	public Employee getEmployee() {
+		/** Gets the current employee.
+		*/
 		return employee;
 	}
 
